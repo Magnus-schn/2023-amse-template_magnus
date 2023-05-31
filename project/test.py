4a3e6fbe7e40e5b2d936311e3b67b39c15ef600f
Magnus-schn <magnus.schneider1998@gmail.com>
2023-05-31 11:10:46 +0200

    Testing

---
+++ project/test.py
@@ -0,0 +1,14 @@
+import os
+import sqlite3
+
+conn = sqlite3.connect('data/database.db')
+
+def check_db_file_exists(conn):
+    if os.path.isfile(conn):
+        print(f"The database file '{conn}' exists.")
+    else:
+        print(f"The database file '{conn}' does not exist.")
+
+# Example usage
+database_file = "data/database.db"
+check_db_file_exists(database_file)
--- project/tests.sh
+++ project/tests.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
-
-# Execute the data pipeline
-python data/data_pipeline.py
-
-# Validate the output files
-if [ -f "data/database.db" ]; then
-    echo "SQLite database file created successfully."
-else
-    echo "Error: SQLite database file not found."
-fi
+echo ~~~ Running Pipeline~~~
+python ../data/data_pipeline.py
+echo ~~~ Executing automated tests for the project. ~~~
+python test.py
+echo ~~~ Testing done. ~~~
